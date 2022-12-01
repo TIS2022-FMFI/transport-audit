115baf5d7e32da55ced789866aa179c98be5ccb2
Roman Božik <107358703+JohnyCarrot@users.noreply.github.com>
2022-12-01 15:29:10 +0100

    pridané testy obsahu vzdialenej db

--- testy/sqlite_test.py
+++ testy/sqlite_test.py
@@ -521,35 +521,298 @@ def over_data_User():
     assert k[3] == User_Role_id2
 
 
+def over_data_Config():
+    for i in range(20):
+        Customer().nahraj("Meno")
+        Vehicle().nahraj("POKEMON")
+    Customer_id = db.execute("SELECT id FROM  Customer ORDER BY RANDOM() LIMIT 1").fetchone()[0]
+    Vehicle_id = db.execute("SELECT id FROM  Vehicle ORDER BY RANDOM() LIMIT 1").fetchone()[0]
+    config = Config().nahraj(Customer_id,Vehicle_id)
+    cur.execute(f""" SELECT * from "Config" where id = '{config.id}' """)
+    k = cur.fetchone()
+    assert k[1] == Customer_id
+    assert k[2] == Vehicle_id
+    Customer_id2 = db.execute("SELECT id FROM  Customer ORDER BY RANDOM() LIMIT 1").fetchone()[0]
+    Vehicle_id2 = db.execute("SELECT id FROM  Vehicle ORDER BY RANDOM() LIMIT 1").fetchone()[0]
+    config.Customer_id = Customer_id2
+    config.Vehicle_id = Vehicle_id2
+    config.update()
+    cur.execute(f""" SELECT * from "Config" where id = '{config.id}' """)
+    k = cur.fetchone()
+    assert k[1] == Customer_id2
+    assert k[2] == Vehicle_id2
 
+def over_data_Shipment():
+    for i in range(20):
+        Customer().nahraj("Meno")
+        Vehicle().nahraj("POKEMON")
+        user_role_id = db.execute("SELECT id FROM  User_Role ORDER BY RANDOM()").fetchone()[0]
+        User().nahraj("Tester","Testovič",user_role_id)
+    user_code = db.execute("SELECT code FROM  User ORDER BY RANDOM()").fetchone()[0]
+    Date_time_close = str(datetime.datetime(2013, 9, 20,13,00))
+    Customer_id = db.execute("SELECT id FROM  Customer ORDER BY RANDOM()").fetchone()[0]
+    Vehicle_id = db.execute("SELECT id FROM  Vehicle ORDER BY RANDOM()").fetchone()[0]
+    shipment = Shipment().nahraj(user_code,Date_time_close,Customer_id, Vehicle_id)
+    cur.execute(f""" SELECT * from "Shipment" where id = '{shipment.id}' """)
+    k = cur.fetchone()
+    assert k[0] == user_code
+    assert str(k[1]) == Date_time_close
+    assert k[2] == shipment.id
+    assert k[3] == Customer_id
+    assert k[4] == Vehicle_id
+    user_code2 = db.execute("SELECT code FROM  User ORDER BY RANDOM()").fetchone()[0]
+    Date_time_close2 = str(datetime.datetime(2013, 9, 20,13,00))
+    Customer_id2 = db.execute("SELECT id FROM  Customer ORDER BY RANDOM()").fetchone()[0]
+    Vehicle_id2 = db.execute("SELECT id FROM  Vehicle ORDER BY RANDOM()").fetchone()[0]
+    shipment.User_code = user_code2
+    shipment.Date_time_close = Date_time_close2
+    shipment.Customer_id = Customer_id2
+    shipment.Vehicle_id = Vehicle_id2
+    shipment.update()
+    cur.execute(f""" SELECT * from "Shipment" where id = '{shipment.id}' """)
+    k = cur.fetchone()
+    assert k[0] == user_code2
+    assert str(k[1]) == Date_time_close2
+    assert k[2] == shipment.id
+    assert k[3] == Customer_id2
+    assert k[4] == Vehicle_id2
 
+def over_data_Pattern():
+    Customer_id = db.execute("SELECT id FROM  Customer ORDER BY RANDOM()").fetchone()[0]
+    pattern = Pattern().nahraj(Customer_id)
+    cur.execute(f""" SELECT * from "Pattern" where id = '{pattern.id}' """)
+    k = cur.fetchone()
+    assert k[0] == pattern.id
+    assert k[1] == Customer_id
+    Customer_id2 = db.execute("SELECT id FROM  Customer ORDER BY RANDOM()").fetchone()[0]
+    pattern.Customer_id = Customer_id2
+    pattern.update()
+    cur.execute(f""" SELECT * from "Pattern" where id = '{pattern.id}' """)
+    k = cur.fetchone()
+    assert k[0] == pattern.id
+    assert k[1] == Customer_id2
 
+def over_data_work_statement():
+    user_code =  db.execute("SELECT code FROM  User ORDER BY RANDOM()").fetchone()[0]
+    work = 5
+    time_start = str(datetime.datetime(2013, 9, 20, 13, 00))
+    time_end = str(datetime.datetime(2015, 9, 20, 13, 00))
+    statement = Work_statement().nahraj(user_code,work,time_start,time_end)
+    cur.execute(f""" SELECT * from "Work_statement" where id = '{statement.id}' """)
+    k = cur.fetchone()
+    assert k[0] == user_code
+    assert k[1] == work
+    assert str(k[2]) == time_start
+    assert str(k[3]) == time_end
+    assert k[4] == statement.id
 
+    user_code2 =  db.execute("SELECT code FROM  User ORDER BY RANDOM()").fetchone()[0]
+    work2 = 52
+    time_start2 = str(datetime.datetime(2113, 9, 20, 13, 00))
+    time_end2 = str(datetime.datetime(2115, 9, 20, 13, 00))
+    statement.User_code = user_code2
+    statement.Work = work2
+    statement.Time_start = time_start2
+    statement.Time_end = time_end2
+    statement.update()
+    cur.execute(f""" SELECT * from "Work_statement" where id = '{statement.id}' """)
+    k = cur.fetchone()
+    assert k[0] == user_code2
+    assert k[1] == work2
+    assert str(k[2]) == time_start2
+    assert str(k[3]) == time_end2
+    assert k[4] == statement.id
 
+def over_data_stillage_type():
+    meno = "Stilidz tajp test"
+    type = Stillage_type().nahraj(meno)
+    cur.execute(f""" SELECT * from "Stillage_type" where id = '{type.id}' """)
+    k = cur.fetchone()
+    assert k[0] == meno
+    assert k[1] == type.id
+    meno2 = "Stilidz tajp test 2 "
+    type.Name = meno2
+    type.update()
+    cur.execute(f""" SELECT * from "Stillage_type" where id = '{type.id}' """)
+    k = cur.fetchone()
+    assert k[0] == meno2
+    assert k[1] == type.id
 
+def over_data_advanced_user():
+    for i in range(20):
+        Customer_id = db.execute("SELECT id FROM  Customer ORDER BY RANDOM()").fetchone()[0]
+        Vehicle_id = db.execute("SELECT id FROM  Vehicle ORDER BY RANDOM()").fetchone()[0]
+        Config().nahraj(Customer_id, Vehicle_id)
 
+    user_code = db.execute("SELECT code FROM  User ORDER BY RANDOM()").fetchone()[0]
+    config_id = db.execute("SELECT id FROM  Config ORDER BY RANDOM()").fetchone()[0]
+    a_user = Advanced_user().nahraj(config_id, user_code)
+    cur.execute(f""" SELECT * from "Advanced_user" where id = '{a_user.id}' """)
+    k = cur.fetchone()
+    assert k[0] == a_user.id
+    assert k[1] == config_id
+    assert k[2] == user_code
+
+    user_code2 = db.execute("SELECT code FROM  User ORDER BY RANDOM()").fetchone()[0]
+    config_id2 = db.execute("SELECT id FROM  Config ORDER BY RANDOM()").fetchone()[0]
+    a_user.User_code = user_code2
+    a_user.Config_id = config_id2
+    a_user.update()
+    cur.execute(f""" SELECT * from "Advanced_user" where id = '{a_user.id}' """)
+    k = cur.fetchone()
+    assert k[0] == a_user.id
+    assert k[1] == config_id2
+    assert k[2] == user_code2
+
+def over_data_Stillage():
+    for i in range(20):
+        user_code = db.execute("SELECT code FROM  User ORDER BY RANDOM()").fetchone()[0]
+        Customer_id = db.execute("SELECT id FROM  Customer ORDER BY RANDOM()").fetchone()[0]
+        Vehicle_id = db.execute("SELECT id FROM  Vehicle ORDER BY RANDOM()").fetchone()[0]
+        Shipment().nahraj(user_code,str(datetime.datetime(2018, 9, 20, 13, 00)), Customer_id,Vehicle_id)
+        Stillage_type().nahraj("Test")
+    Date_time_start = str(datetime.datetime(2017, 9, 20, 13, 00))
+    Date_time_end = str(datetime.datetime(2018, 8, 20, 13, 00))
+    Stillage_number = 5
+    Stillage_number_on_head = 20
+    First_scan_product = 14
+    Last_scan_product = 18
+    JLR_header = 80
+    Carriage_L = "niečo"
+    check = 17
+    First_scan_TLS = 10222
+    Last_scan_TLS = 65222
+    stylage_type_id = db.execute("SELECT id FROM  Stillage_type ORDER BY RANDOM()").fetchone()[0]
+    Shipment_id = db.execute("SELECT id FROM  Shipment ORDER BY RANDOM()").fetchone()[0]
+    TLS_range_start = 18000
+    TLS_range_stop = 185697
+    Note = "Noted Note"
+    stillage = Stillage().nahraj(Date_time_start, Date_time_end, Stillage_number, Stillage_number_on_head, First_scan_product,
+                                 Last_scan_product, JLR_header, Carriage_L,check, First_scan_TLS, Last_scan_TLS,stylage_type_id,Shipment_id, TLS_range_start
+                                 , TLS_range_stop, Note)
+    cur.execute(f""" SELECT * from "Stillage" where id = '{stillage.id}' """)
+    k = cur.fetchone()
+    assert str(k[0]) == Date_time_start
+    assert str(k[1]) == Date_time_end
+    assert k[2] == Stillage_number
+    assert k[3] == Stillage_number_on_head
+    assert k[4] == First_scan_product
+    assert k[5] == Last_scan_product
+    assert k[6] == JLR_header
+    assert k[7] == Carriage_L
+    assert k[8] == check
+    assert k[9] == First_scan_TLS
+    assert k[10] == Last_scan_TLS
+    assert k[11] == stillage.id
+    assert k[12] == stylage_type_id
+    assert k[13] == Shipment_id
+    assert k[14] == TLS_range_start
+    assert k[15] == TLS_range_stop
+    assert k[16] == Note
+
+    Date_time_start2 = str(datetime.datetime(2027, 9, 20, 13, 00))
+    Date_time_end2 = str(datetime.datetime(2038, 8, 20, 13, 00))
+    Stillage_number2 = 54
+    Stillage_number_on_head2 = 203
+    First_scan_product2 = 149
+    Last_scan_product2 = 187
+    JLR_header2 = 808
+    Carriage_L2 = "niečo naniečo"
+    check2 = 178
+    First_scan_TLS2 = 102227
+    Last_scan_TLS2 = 652224
+    stylage_type_id2 = db.execute("SELECT id FROM  Stillage_type ORDER BY RANDOM()").fetchone()[0]
+    Shipment_id2 = db.execute("SELECT id FROM  Shipment ORDER BY RANDOM()").fetchone()[0]
+    TLS_range_start2 = 180004
+    TLS_range_stop2 = 18569725
+    Note2 = "Noted Note Noted"
+    stillage.Date_time_start = Date_time_start2
+    stillage.Date_time_end = Date_time_end2
+    stillage.Stillage_number = Stillage_number2
+    stillage.Stillage_Number_on_Header = Stillage_number_on_head2
+    stillage.First_scan_product = First_scan_product2
+    stillage.Last_scan_product = Last_scan_product2
+    stillage.JLR_Header_NO = JLR_header2
+    stillage.Carriage_L_JLR_H = Carriage_L2
+    stillage._Check = check2
+    stillage.First_scan_TLS_code = First_scan_TLS2
+    stillage.Last_scan_TLS_code = Last_scan_TLS2
+    stillage.Stillage_Type_id = stylage_type_id2
+    stillage.Shipment_id = Shipment_id2
+    stillage.TLS_range_start = TLS_range_start2
+    stillage.TLS_range_stop = TLS_range_stop2
+    stillage.Note = Note2
+    stillage.update()
+    cur.execute(f""" SELECT * from "Stillage" where id = '{stillage.id}' """)
+    k = cur.fetchone()
+    assert str(k[0]) == Date_time_start2
+    assert str(k[1]) == Date_time_end2
+    assert k[2] == Stillage_number2
+    assert k[3] == Stillage_number_on_head2
+    assert k[4] == First_scan_product2
+    assert k[5] == Last_scan_product2
+    assert k[6] == JLR_header2
+    assert k[7] == Carriage_L2
+    assert k[8] == check2
+    assert k[9] == First_scan_TLS2
+    assert k[10] == Last_scan_TLS2
+    assert k[11] == stillage.id
+    assert k[12] == stylage_type_id2
+    assert k[13] == Shipment_id2
+    assert k[14] == TLS_range_start2
+    assert k[15] == TLS_range_stop2
+    assert k[16] == Note2
+
+def over_data_Pattern_Item():
+    for i in range(20):
+        Customer_id = db.execute("SELECT id FROM  Customer ORDER BY RANDOM()").fetchone()[0]
+        Pattern().nahraj(Customer_id)
+    item = Pattern_Item()
+    stylage_type_id = db.execute("SELECT id FROM  Stillage_type ORDER BY RANDOM()").fetchone()[0]
+    pattern_id = db.execute("SELECT id FROM  Pattern ORDER BY RANDOM()").fetchone()[0]
+    number = 65
+    item.nahraj(number,pattern_id,stylage_type_id)
+    cur.execute(f""" SELECT * from "Pattern_Item" where id = '{item.id}' """)
+    k = cur.fetchone()
+    assert k[0] == number
+    assert k[1] == item.id
+    assert k[2] == pattern_id
+    assert k[3] == stylage_type_id
+    stylage_type_id2 = db.execute("SELECT id FROM  Stillage_type ORDER BY RANDOM()").fetchone()[0]
+    pattern_id2 = db.execute("SELECT id FROM  Pattern ORDER BY RANDOM()").fetchone()[0]
+    number2 = 652
+    item.Stillage_type_id = stylage_type_id2
+    item.Pattern_id = pattern_id2
+    item.Number = number2
+    item.update()
+    cur.execute(f""" SELECT * from "Pattern_Item" where id = '{item.id}' """)
+    k = cur.fetchone()
+    assert k[0] == number2
+    assert k[1] == item.id
+    assert k[2] == pattern_id2
+    assert k[3] == stylage_type_id2
 
 if __name__ == '__main__':
     # cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
     # print(cursor.fetchall())
     start_time = time.time()
     #Test obsahu tried
-    # vyprazdni_db()
-    # General_test()
-    # User_role_test()
-    # Customer_test()
-    # Vehicle_test()
-    # User_test()
-    # Config_test()
-    # Shipment_test()
-    # Pattern_test()
-    # Work_statement_test()
-    # Stillage_type_test()
-    # Advanced_user_test()
-    # Stillage_test()
-    # Pattern_item_test()
-    # print("TEST OBSAHU TRIED OK - trval %s sekúnd" % (time.time() - start_time))
-    #Test obsahu lokálnej databázy
+    vyprazdni_db()
+    General_test()
+    User_role_test()
+    Customer_test()
+    Vehicle_test()
+    User_test()
+    Config_test()
+    Shipment_test()
+    Pattern_test()
+    Work_statement_test()
+    Stillage_type_test()
+    Advanced_user_test()
+    Stillage_test()
+    Pattern_item_test()
+    print("TEST OBSAHU A ŠTRUKTÚRY TRIED OK - trval %s sekúnd" % (time.time() - start_time))
+    #Test obsahu vzdialenej databázy
     start_time = time.time()
     last_sync_db_test()
     over_data_general()
@@ -557,6 +820,14 @@ if __name__ == '__main__':
     over_data_Customer()
     over_data_Vehicle()
     over_data_User()
-
-    print("TEST OBSAHU SQLITE OK - trval %s sekúnd" % (time.time() - start_time))
-    # Test obsahu vzdialenej datbázy
+    over_data_Config()
+    over_data_Shipment()
+    over_data_Pattern()
+    over_data_work_statement()
+    over_data_stillage_type()
+    over_data_advanced_user()
+    over_data_Stillage()
+    over_data_Pattern_Item()
+    vyprazdni_db()
+    print("TEST OBSAHU DB OK - trval %s sekúnd" % (time.time() - start_time))
+    #Pridať Test synchronizácie
