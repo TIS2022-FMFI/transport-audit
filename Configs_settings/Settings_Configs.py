a60e63e75d9f69e3ce3cf90ae5a42ab415d26a90
JozefKolek <100275065+JozefKolek@users.noreply.github.com>
2022-12-21 19:22:19 +0100

    Odstraneny edit

--- Configs_settings/Settings_Configs.py
+++ Configs_settings/Settings_Configs.py
@@ -1,12 +1,10 @@
 from Configs_settings.Delete_Configs import *
 from Configs_settings.Add_Configs import *
-from Configs_settings.Edit_Configs import *
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.app import App
 class Settings_Configs (BoxLayout):
     btn1 = Button(text="Pridaj")
-    # btn2 = Button(text="Uprav")
     btn3 = Button(text="Vymaz")
     btn4 = Button(text="Spat")
     screenManager = None
@@ -15,19 +13,15 @@ class Settings_Configs (BoxLayout):
         super().__init__()
         self.screenManager = screenManager
         self.btn1.bind(on_release = lambda btn: self.call_add())
-        # self.btn2.bind(on_release=lambda btn: self.call_edit())
         self.btn3.bind(on_release=lambda btn: self.call_delete())
         self.btn4.bind(on_release=lambda btn: self.call_back())
         self.add_widget(self.btn1)
-        # self.add_widget(self.btn2)
         self.add_widget(self.btn3)
         self.add_widget(self.btn4)
     def call_add(self):
         self.screenManager.current = 'Add_Configs'
-    # def call_edit(self):
-    #     self.screenManager.current = 'Edit_Configs'
+
     def call_delete(self):
         self.screenManager.current = 'Delete_Configs'
     def call_back(self):
         self.screenManager.current = 'Menu_screen'
-
