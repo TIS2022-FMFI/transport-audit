c454c26921859d5493e0f20754679f0a5f995d96
MichaelU6 <72891693+MichaelU6@users.noreply.github.com>
2022-12-28 15:40:00 +0100

    Add files via upload

--- Export_setting/Settings_export.py
+++ Export_setting/Settings_export.py
@@ -12,18 +12,19 @@ class Settings_Exports (BoxLayout):
     btn1 = Button(text="Nastav")
     btn2 = Button(text="Späť")
     screenManager = None
+    values=[]
     def __init__(self, screenManager,**kwargs):
         super(Settings_Exports, self).__init__(**kwargs)
         self.screenManager = screenManager
-        mainbutton = Button(text='Vyber autoexport', size_hint=(.5, .25),pos=(60, 20))
-        mainbutton.bind(on_release=self.drop1.open)
-        self.drop1.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
-        self.btn1.bind(on_release = lambda btn:self.check())
-        self.btn2.bind(on_release=lambda btn: self.call_Back())
-        self.add_widget(mainbutton)
-        self.add_widget(self.btn1)
-        self.add_widget(self.btn2)
-        self.add_widget(self.notify)
+        # mainbutton = Button(text='Vyber autoexport', size_hint=(.5, .25),pos=(60, 20))
+        # mainbutton.bind(on_release=self.drop1.open)
+        # self.drop1.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+        # self.btn1.bind(on_release = lambda btn:self.check())
+        # self.btn2.bind(on_release=lambda btn: self.call_Back())
+        # self.add_widget(mainbutton)
+        # self.add_widget(self.btn1)
+        # self.add_widget(self.btn2)
+        # self.add_widget(self.notify)
     def set_selected(self,text):
         self.selected_answer = 0
         if text == "Yes":
@@ -40,17 +41,21 @@ class Settings_Exports (BoxLayout):
             self.call_Back()
     def clear_screen(self,*args):
         self.notify.text = ""
-        self.drop1.clear_widgets()
+        # self.drop1.clear_widgets()
         id = General().vrat_vsetky()[-1]['Automatic_export']
         for i in ["Yes","No"]:
-            btn = Button(text=i, size_hint_y=None, height=40,on_release=lambda btn: self.set_selected(btn.text))
-            btn.bind(on_release=lambda btn: self.drop1.select(btn.text))
-            self.drop1.add_widget(btn)
-        self.drop1.select('Vyber autoexport')
+            # btn = Button(text=i, size_hint_y=None, height=40,on_release=lambda btn: self.set_selected(btn.text))
+            # btn.bind(on_release=lambda btn: self.drop1.select(btn.text))
+            # self.drop1.add_widget(btn)
+            self.values.append(i)
+        self.ids.spinner_export.values = self.values
+        # self.drop1.select('Vyber autoexport')
         self.selected_answer = None
         if id == 0:
-            self.drop1.select("No")
+            # self.drop1.select("No")
+            self.ids.spinner_export.text = "No"
             self.selected_answer = 1
         if id == 1:
-            self.drop1.select("Yes")
+            # self.drop1.select("Yes")
+            self.ids.spinner_export.text= "Yes"
             self.selected_answer = 0
