49478be77113bf1c62a1c443645a9c0a16657d46
Roman Božik <107358703+JohnyCarrot@users.noreply.github.com>
2022-12-29 14:24:03 +0100

    Add files via upload

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ def citaj_report():
         reader_obj = csv.reader(file_obj)
         pointer = 0
         hlava = []
-        vysledok = []
+        vysledok = {}
         for row in reader_obj:
             if pointer==0:
                 hlava = row[0].split(",")
@@ -15,12 +15,27 @@ def citaj_report():
                 for slovo in row[0].split(","):
                     k[hlava[pointrik]] = slovo
                     pointrik+=1
-                vysledok.append(k)
+                if k['carriagefullno'] not in vysledok:
+                    vysledok[k['carriagefullno']] = []
+                vysledok[k['carriagefullno']].append(k)
 
             pointer +=1
+    for polozkyVozika in vysledok.values(): #ak nahodou v reporte nie su polozky vozika v spravnom poradi
+        polozkyVozika.sort(key = lambda x : int(x['position']))
     return vysledok
 
 if __name__ == "__main__" :
-    print(citaj_report())
+    vysl = citaj_report()
+    print(len(vysl))
+
+    for k, v in vysl.items():
+        print(k)
+        for vv in v:
+            print(vv)
+        print()
+
+    print("\n\n")
+    print(vysl['0113015417677'][0])
+    print(vysl['0113015417677'][-1])
 
 
