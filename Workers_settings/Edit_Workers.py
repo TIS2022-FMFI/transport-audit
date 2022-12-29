cb9ffaea8100f798e7b14c50ece0c0bf1d6e00fe
MichaelU6 <72891693+MichaelU6@users.noreply.github.com>
2022-12-29 13:30:27 +0100

    gui

--- Workers_settings/Edit_Workers.py
+++ Workers_settings/Edit_Workers.py
@@ -18,45 +18,56 @@ class Edit_Workers (BoxLayout):
     btn3 = Button(text="Uzivatelsky kod")
     workers_list = None
     screenManager = None
+    values1=[]
+    values2=[]
     def __init__(self, screenManager,**kwargs):
         super(Edit_Workers, self).__init__(**kwargs)
         self.screenManager = screenManager
-        mainbutton = Button(text='Vyber rolu', size_hint=(.5, .25),pos=(60, 20))
-        mainbutton.bind(on_release=self.drop1.open)
-        mainbutton1 = Button(text='Vyber pracovnika na upravu', size_hint=(.5, .25), pos=(60, 20))
-        mainbutton1.bind(on_release=self.drop2.open)
-        self.drop1.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
-        self.drop2.bind(on_select=lambda instance, x: setattr(mainbutton1, 'text', x))        
-        self.btn1.bind(on_release = lambda btn:self.check())
-        self.btn2.bind(on_release=lambda btn: self.call_Back())
-        self.add_widget(mainbutton1)
-        self.add_widget(self.btn3)
-        self.add_widget(self.text1)
-        self.add_widget(self.text2)
-        self.add_widget(mainbutton)
-        self.add_widget(self.btn1)
-        self.add_widget(self.btn2)
-        self.add_widget(self.notify)
+        self.text1 = self.ids.input_edit_worker_1
+        self.text2 = self.ids.input_edit_worker_2
+        # mainbutton = Button(text='Vyber rolu', size_hint=(.5, .25),pos=(60, 20))
+        # mainbutton.bind(on_release=self.drop1.open)
+        # mainbutton1 = Button(text='Vyber pracovnika na upravu', size_hint=(.5, .25), pos=(60, 20))
+        # mainbutton1.bind(on_release=self.drop2.open)
+        # self.drop1.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+        # self.drop2.bind(on_select=lambda instance, x: setattr(mainbutton1, 'text', x))
+        # self.btn1.bind(on_release = lambda btn:self.check())
+        # self.btn2.bind(on_release=lambda btn: self.call_Back())
+        # self.add_widget(mainbutton1)
+        # self.add_widget(self.btn3)
+        # self.add_widget(self.text1)
+        # self.add_widget(self.text2)
+        # self.add_widget(mainbutton)
+        # self.add_widget(self.btn1)
+        # self.add_widget(self.btn2)
+        # self.add_widget(self.notify)
     def synchronize_workers(self):
         self.select_code = None
-        self.drop2.clear_widgets()
-        self.drop2.select("Vyber pracovnika na upravu")
+        # self.drop2.clear_widgets()
+        # self.drop2.select("Vyber pracovnika na upravu")
         self.workers_list = dict([(i['Name'][0] + ". " + i['Last_name'] + " " + str(i['code']), str(i['code'])) for i in User().vrat_vsetky() if i['doplnok'] != 'DELETED'])
         for i in self.workers_list:
-            btn = Button(text= i, size_hint_y=None, height=40, on_release=lambda btn: self.set_widgets(self.workers_list[btn.text]))
-            btn.bind(on_release=lambda btn: self.drop2.select(btn.text))
-            self.drop2.add_widget(btn)
+            # btn = Button(text= i, size_hint_y=None, height=40, on_release=lambda btn: self.set_widgets(self.workers_list[btn.text]))
+            # btn.bind(on_release=lambda btn: self.drop2.select(btn.text))
+            # self.drop2.add_widget(btn)
+            self.values2.append(i)
+        self.ids.spinner_edit_worker_1.values = self.values2
+        # self.ids.spinner_edit_worker_1.text = 'Vyber pracovnika na upravu'
     def synchronize_user_roles(self):
         self.select_code = None
-        self.drop1.clear_widgets()
-        self.drop1.select('Vyber rolu')
+        # self.drop1.clear_widgets()
+        # self.drop1.select('Vyber rolu')
         for i in User_Role().vrat_vsetky():
-            btn = Button(text=i["name"], size_hint_y=None, height=40,on_release = lambda btn: self.set_selected(btn.text))
-            btn.bind(on_release=lambda btn: self.drop1.select(btn.text))
-            self.drop1.add_widget(btn)
+            # btn = Button(text=i["name"], size_hint_y=None, height=40,on_release = lambda btn: self.set_selected(btn.text))
+            # btn.bind(on_release=lambda btn: self.drop1.select(btn.text))
+            # self.drop1.add_widget(btn)
+            self.values1.append(i["name"])
+        self.ids.spinner_edit_worker_2.values = self.values1
+        self.ids.spinner_edit_worker_2.text = 'Vyber rolu'
     def set_select(self,tex):
         self.select_role = tex
-    def set_widgets(self,tex1):
+    def set_widgets(self,text):
+        tex1 = self.workers_list[text]
         select_user = User().stiahni(int(tex1))
         self.text1.text = select_user.Name
         self.text2.text = select_user.Last_name
