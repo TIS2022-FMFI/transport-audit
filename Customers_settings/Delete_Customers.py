2410ba3bf0658d168e8403e4614fd61821db338c
MichaelU6 <72891693+MichaelU6@users.noreply.github.com>
2022-12-29 13:29:02 +0100

    gui

--- Customers_settings/Delete_Customers.py
+++ Customers_settings/Delete_Customers.py
@@ -12,27 +12,30 @@ class Delete_Customers (BoxLayout):
     btn2 = Button(text="Späť")
     customer_list = None
     screenManager = None
+    values=[]
     def synchronize_customers(self):
         self.on_delete_selected = None
         self.customer_list = dict([(i['Name'],i['id']) for i in Customer().vrat_vsetky() if i['doplnok'] != 'DELETED'])
-        self.drop1.clear_widgets()
-        self.drop1.select("Vyber pracovnika na vymazanie")
+        # self.drop1.clear_widgets()
+        # self.drop1.select("Vyber pracovnika na vymazanie")
         for i in self.customer_list:
-            btn = Button(text= i, size_hint_y=None, height=40, on_release=lambda btn: self.select_code(btn.text))
-            btn.bind(on_release=lambda btn: self.drop1.select(btn.text))
-            self.drop1.add_widget(btn)
+            # btn = Button(text= i, size_hint_y=None, height=40, on_release=lambda btn: self.select_code(btn.text))
+            # btn.bind(on_release=lambda btn: self.drop1.select(btn.text))
+            # self.drop1.add_widget(btn)
+            self.values.append(i)
+        self.ids.spinner_delete_customer.values = self.values
     def __init__(self, screenManager,**kwargs):
         super(Delete_Customers, self).__init__(**kwargs)
         self.screenManager = screenManager
-        mainbutton1 = Button(text='Vyber pracovnika na vymazanie', size_hint=(.5, .25), pos=(60, 20))
-        mainbutton1.bind(on_release=self.drop1.open)
-        self.drop1.bind(on_select=lambda instance, x: setattr(mainbutton1, 'text', x))
-        self.btn1.bind(on_release = lambda btn:self.check())
-        self.btn2.bind(on_release=lambda btn: self.call_Back())
-        self.add_widget(mainbutton1)
-        self.add_widget(self.btn1)
-        self.add_widget(self.btn2)
-        self.add_widget(self.notify)
+        # mainbutton1 = Button(text='Vyber pracovnika na vymazanie', size_hint=(.5, .25), pos=(60, 20))
+        # mainbutton1.bind(on_release=self.drop1.open)
+        # self.drop1.bind(on_select=lambda instance, x: setattr(mainbutton1, 'text', x))
+        # self.btn1.bind(on_release = lambda btn:self.check())
+        # self.btn2.bind(on_release=lambda btn: self.call_Back())
+        # self.add_widget(mainbutton1)
+        # self.add_widget(self.btn1)
+        # self.add_widget(self.btn2)
+        # self.add_widget(self.notify)
     def select_code(self,tex):
         self.on_delete_selected = self.customer_list[tex]
     def call_Back (self):
