249aaf85fa797a0337423d763a94e06942c0308f
murodrap <tstrisovska@gmail.com>
2022-12-29 18:07:17 +0100

    doplnena funkcia v parseri

---
+++ parser.py
@@ -0,0 +1,59 @@
+import csv
+
+def citaj_report():
+    with open('report.csv') as file_obj:
+        reader_obj = csv.reader(file_obj)
+        pointer = 0
+        hlava = []
+        vysledok = []
+        for row in reader_obj:
+            if pointer==0:
+                hlava = row[0].split(",")
+            else:
+                pointrik = 0
+                k = dict()
+                for slovo in row[0].split(","):
+                    k[hlava[pointrik]] = slovo
+                    pointrik+=1
+                vysledok.append(k)
+
+            pointer +=1
+    return vysledok
+
+def citaj_report_dict():
+    with open('report.csv') as file_obj:
+        reader_obj = csv.reader(file_obj)
+        pointer = 0
+        hlava = []
+        vysledok = {}
+        for row in reader_obj:
+            if pointer==0:
+                hlava = row[0].split(",")
+            else:
+                pointrik = 0
+                k = dict()
+                for slovo in row[0].split(","):
+                    k[hlava[pointrik]] = slovo
+                    pointrik+=1
+                if k['carriagefullno'] not in vysledok:
+                    vysledok[k['carriagefullno']] = []
+                vysledok[k['carriagefullno']].append(k)
+
+            pointer +=1
+    for polozkyVozika in vysledok.values(): #ak nahodou v reporte nie su polozky vozika v spravnom poradi
+        polozkyVozika.sort(key = lambda x : int(x['position']))
+    return vysledok
+
+if __name__ == "__main__" :
+    vysl = citaj_report_dict()
+    print(len(vysl))
+
+    for k, v in vysl.items():
+        print(k)
+        print(v[0])
+        print(v[-1])
+        print()
+
+
+
+
