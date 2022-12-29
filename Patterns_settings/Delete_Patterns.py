dc19332c30e296a53fdfe68d2736ae4f31e820d2
MichaelU6 <72891693+MichaelU6@users.noreply.github.com>
2022-12-29 13:29:49 +0100

    gui

--- Patterns_settings/Delete_Patterns.py
+++ Patterns_settings/Delete_Patterns.py
@@ -16,30 +16,33 @@ class Delete_Patterns (BoxLayout):
     pattern_list = None
     Edit_data = None
     screenManager = None
+    values=[]
     def __init__(self,screenManager, **kwargs):
         super(Delete_Patterns, self).__init__(**kwargs)
         self.screenManager = screenManager
-        mainbutton2 = Button(text='Customer', size_hint=(.5, .25), pos=(60, 20))
-        mainbutton2.bind(on_release=self.drop2.open)
-        self.drop2.bind(on_select=lambda instance, x: setattr(mainbutton2, 'text', x))
-        self.btn1.bind(on_release = lambda btn:self.check())
-        self.btn2.bind(on_release=lambda btn: self.call_Back())
-        self.add_widget(mainbutton2)
-        # self.add_widget(mainbutton1)
-        self.add_widget(self.btn1)
-        self.add_widget(self.btn2)
-        self.add_widget(self.notify)
+        # mainbutton2 = Button(text='Customer', size_hint=(.5, .25), pos=(60, 20))
+        # mainbutton2.bind(on_release=self.drop2.open)
+        # self.drop2.bind(on_select=lambda instance, x: setattr(mainbutton2, 'text', x))
+        # self.btn1.bind(on_release = lambda btn:self.check())
+        # self.btn2.bind(on_release=lambda btn: self.call_Back())
+        # self.add_widget(mainbutton2)
+        # # self.add_widget(mainbutton1)
+        # self.add_widget(self.btn1)
+        # self.add_widget(self.btn2)
+        # self.add_widget(self.notify)
 
     def synchronize_customers(self):
         self.select_customer = None
-        self.drop2.clear_widgets()
-        self.drop2.select("Customer")
+        # self.drop2.clear_widgets()
+        # self.drop2.select("Customer")
         self.customer_list = dict([(i['Name'], i['id']) for i in Customer().vrat_vsetky() if i['doplnok'] != 'DELETED'])
         for i in self.customer_list:
             if self.customer_list[i] in self.pattern_list.values():
-                btn = Button(text=i, size_hint_y=None, height=40, on_release=lambda btn: self.set_customer_id(btn.text))
-                btn.bind(on_release=lambda btn: self.drop2.select(btn.text))
-                self.drop2.add_widget(btn)
+                # btn = Button(text=i, size_hint_y=None, height=40, on_release=lambda btn: self.set_customer_id(btn.text))
+                # btn.bind(on_release=lambda btn: self.drop2.select(btn.text))
+                # self.drop2.add_widget(btn)
+                self.values.append(i)
+        self.ids.spinner_delete_patter.values = self.values
     def synchronize_patterns(self):
         self.select_pattern = None
         self.pattern_list = dict([(i['id'], i['Customer_id']) for i in Pattern().vrat_vsetky() if i['doplnok'] != 'DELETED'])
