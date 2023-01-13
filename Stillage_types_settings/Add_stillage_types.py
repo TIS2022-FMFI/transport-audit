43b65bac974c70a5dd9d9d90c3b0bef1a94cc3e4
MichaelU6 <72891693+MichaelU6@users.noreply.github.com>
2023-01-13 11:32:31 +0100

    Add files via upload

--- Stillage_types_settings/Add_stillage_types.py
+++ Stillage_types_settings/Add_stillage_types.py
@@ -12,15 +12,17 @@ class Add_Stillage_type (BoxLayout):
     def __init__(self,screenManager, **kwargs):
         super(Add_Stillage_type, self).__init__(**kwargs)
         self.screenManager = screenManager
-        self.text1 = TextInput(text = 'Meno typu vozika')
-        btn1 = Button(text="Pridaj")
-        btn1.bind(on_release = lambda btn:self.check())
-        btn2 = Button(text="Späť")
-        btn2.bind(on_release=lambda btn: self.call_Back())
-        self.add_widget(self.text1)
-        self.add_widget(btn1)
-        self.add_widget(btn2)
-        self.add_widget(self.notify)
+        # self.text1 = TextInput(text = 'Meno typu vozika')
+        # btn1 = Button(text="Pridaj")
+        # btn1.bind(on_release = lambda btn:self.check())
+        # btn2 = Button(text="Späť")
+        # btn2.bind(on_release=lambda btn: self.call_Back())
+        # self.add_widget(self.text1)
+        # self.add_widget(btn1)
+        # self.add_widget(btn2)
+        # self.add_widget(self.notify)
+        self.text1 = self.ids.input_add_st
+        self.notify = self.ids.notify
     def synchronize_stillage_types(self):
         self.stillage_type_list = [i['Name'] for i in Stillage_type().vrat_vsetky() if i['doplnok'] != 'DELETED']
     def call_Back (self):
@@ -36,5 +38,5 @@ class Add_Stillage_type (BoxLayout):
             self.call_Back()
     def clear_screen(self, *args):
         self.notify.text = ""
-        self.text1.text = 'Meno typu vozika'
+        self.text1.text = ''
         self.synchronize_stillage_types()
--- Stillage_types_settings/Edit__stillage_types.py
+++ Stillage_types_settings/Edit__stillage_types.py
@@ -13,29 +13,38 @@ class Edit_Stillage_type (BoxLayout):
     btn2 = Button(text="Späť")
     stillage_type_list = None
     screenManager = None
+    values = []
     def synchronize_stillage_types(self):
         self.select_id = None
-        self.drop1.clear_widgets()
-        self.drop1.select("Vyber stillage type na upravu")
+        # self.drop1.clear_widgets()
+        # self.drop1.select("Vyber stillage type na upravu")
+        self.values = []
         self.stillage_type_list = dict([(i['Name'], i['id']) for i in Stillage_type().vrat_vsetky() if i['doplnok'] != 'DELETED'])
         for i in self.stillage_type_list:
-            btn = Button(text= i, size_hint_y=None, height=40, on_release=lambda btn: self.set_widgets(btn.text))
-            btn.bind(on_release=lambda btn: self.drop1.select(btn.text))
-            self.drop1.add_widget(btn)
+            # btn = Button(text= i, size_hint_y=None, height=40, on_release=lambda btn: self.set_widgets(btn.text))
+            # btn.bind(on_release=lambda btn: self.drop1.select(btn.text))
+            # self.drop1.add_widget(btn)
+            self.values.append(i)
+        self.ids.spinner_edit_st.values = self.values
+        self.ids.spinner_edit_st.text = "Vyber stillage type na upravu"
     def __init__(self,screenManager, **kwargs):
         super(Edit_Stillage_type, self).__init__(**kwargs)
         self.screenManager = screenManager
-        mainbutton1 = Button(text='Vyber stillage type na upravu', size_hint=(.5, .25), pos=(60, 20))
-        mainbutton1.bind(on_release=self.drop1.open)
-        self.drop1.bind(on_select=lambda instance, x: setattr(mainbutton1, 'text', x))
-        self.btn1.bind(on_release = lambda btn:self.check())
-        self.btn2.bind(on_release=lambda btn: self.call_Back())
-        self.add_widget(mainbutton1)
-        self.add_widget(self.text1)
-        self.add_widget(self.btn1)
-        self.add_widget(self.btn2)
-        self.add_widget(self.notify)
+        # mainbutton1 = Button(text='Vyber stillage type na upravu', size_hint=(.5, .25), pos=(60, 20))
+        # mainbutton1.bind(on_release=self.drop1.open)
+        # self.drop1.bind(on_select=lambda instance, x: setattr(mainbutton1, 'text', x))
+        # self.btn1.bind(on_release = lambda btn:self.check())
+        # self.btn2.bind(on_release=lambda btn: self.call_Back())
+        # self.add_widget(mainbutton1)
+        # self.add_widget(self.text1)
+        # self.add_widget(self.btn1)
+        # self.add_widget(self.btn2)
+        # self.add_widget(self.notify)
+        self.text1 = self.ids.input_edit_st
+        self.notify = self.ids.notify
     def set_widgets(self,tex1):
+        if tex1 not in self.stillage_type_list:
+            return
         self.text1.text = tex1
         self.select_id = self.stillage_type_list[tex1]
     def call_Back (self):
@@ -53,6 +62,6 @@ class Edit_Stillage_type (BoxLayout):
             updated_stillage_type.update()
             self.call_Back()
     def clear_screen(self, *args):
-        self.text1.text = "Meno typu vozika"
+        self.text1.text = ""
         self.notify.text = ""
         self.synchronize_stillage_types()
